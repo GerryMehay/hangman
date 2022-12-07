9ed493cebb3f54f7a99a004d14fe8f55965fcfb0
GerryMehay <117312778+GerryMehay@users.noreply.github.com>
2022-12-07 09:50:04 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -1,6 +1,33 @@
 #%%
 import random
 
+guess = input("Enter a single letter.")
+
+word_list = ['banana','apple','orange','melon','pear'] 
+
+word = random.choice(word_list)
+
+
+
+def check_guess(guess):
+
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} in not in the word. Try again.")
+
+def ask_for_input():
+    while True: 
+        guess = input("Enter a single letter.")
+        guess = guess.lower()
+        if len(guess) ==1 and guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetic character")
+    check_guess(guess)
+
+ask_for_input()
+
 class Hangman:
     def __init__(self, word_list=['banana','apple','orange','melon','pear'], num_lives=5):
         self.word = random.choice(word_list)
